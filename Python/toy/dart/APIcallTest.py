a63f76a68579480c4f57507f2868bdaabbc0729c
Gros19 <kjj6393@gmail.com>
2022-01-16 20:15:57 +0900

    16일차 작업

--- Python/toy/dart/APIcallTest.py
+++ Python/toy/dart/APIcallTest.py
@@ -3,7 +3,9 @@ import xml.etree.ElementTree as ET
 import zipfile
 import io
 import os, sys
+import pandas as pd
 #상위의 상위 폴더라서 두번 사용
+#KhAPI-Python-toy/APIcallTest.py
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 from src.utils import get_project_root
 
@@ -12,48 +14,69 @@ saveFileDir = f"{get_project_root()}\\data\\dart\\corpcode"
 print("회사코드 저장 경로: ", saveFileDir)
 
 API_KEY = "a27a4e6bf5d8701810d49839d64922a438b03918"
-url = "https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key="
 
-with urlopen(url+API_KEY) as zipresp:
-    with io.BytesIO(zipresp.read()) as ioB:
-        with zipfile.ZipFile(ioB) as zip:
-            FILENAME= zip.namelist()[0]
-            zip.extractall(saveFileDir)
-
+def down_corp_codeXML(API_KEY, saveFileDir):
+    url = "https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key="
+    with urlopen(url+API_KEY) as zipresp:
+        with io.BytesIO(zipresp.read()) as ioB:
+            with zipfile.ZipFile(ioB) as zip:
+                zip.extractall(saveFileDir)
+                
     """
     <result>
-    <list>
-        <corp_code>00434003</corp_code>
-        <corp_name>다코</corp_name>
-        <stock_code> </stock_code>
-        <modify_date>20170630</modify_date>
-    </list>
-    <list>
-        <corp_code>00434456</corp_code>
-        <corp_name>일산약품</corp_name>
-        <stock_code> </stock_code>
-        <modify_date>20170630</modify_date>
-    </list>
-    <list>
-        <corp_code>00430964</corp_code>
-        <corp_name>굿앤엘에스</corp_name>
-        <stock_code> </stock_code>
-        <modify_date>20170630</modify_date>
-    </list>
-    <result>
+        <list>
+            <corp_code>00434003</corp_code>
+            <corp_name>다코</corp_name>
+            <stock_code> </stock_code>
+            <modify_date>20170630</modify_date>
+        </list>
+        <list>
+            <corp_code>00434456</corp_code>
+            <corp_name>일산약품</corp_name>
+            <stock_code> </stock_code>
+            <modify_date>20170630</modify_date>
+        </list>
+        <list>
+            <corp_code>00430964</corp_code>
+            <corp_name>굿앤엘에스</corp_name>
+            <stock_code> </stock_code>
+            <modify_date>20170630</modify_date>
+        </list>
+    </result>
     """
 
-xml_file = f"{saveFileDir}\{FILENAME}"
-print(xml_file)
-doc = ET.parse(xml_file).getroot()
-list_tg = doc.findall("list")
 
-for i in list_tg:
-    if i.find("stock_code").text != " ":
-        print(i.find("corp_code").text, i.find("stock_code").text, i.find("corp_name").text,sep='\t')
+def parse_corp_code(saveFileDir):
+    a = []
+    b = []
+    c = []
+    
+    xml_file = f"{saveFileDir}\CORPCODE.xml"
+    print(xml_file)
+    doc = ET.parse(xml_file).getroot()
+    list_tg = doc.findall("list")
 
-
-
-
+    
+    for i in list_tg:
+        # if i.find("stock_code").text != " ":
+        #     print(i.find("stock_code").text)
+        a.append(i.find("corp_code").text)
+        c.append(i.find("corp_name").text)
+        if i.find("stock_code").text == " ":
+            b.append("99999999")
+        else:
+            b.append(i.find("stock_code").text)
             
-            
+    res = [a, b, c]
+    return res  
+
+
+abc = parse_corp_code(saveFileDir)
+good =list(zip(abc[0], abc[1], abc[2]))
+
+# for a in good:
+#     print(a)
+
+p1 = pd.Series(good)
+
+print(p1)
---
+++ Python/toy/stuff/argv_test.py
@@ -0,0 +1,4 @@
+import sys
+res = sys.argv
+for i in res[1:]:
+    print(i)

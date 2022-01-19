de3fe58e4ca183fd725111ccde1e4498f1a75643
Gros19 <kjj6393@gmail.com>
2022-01-19 14:13:25 +0900

    [변경 및 구조 개선] api_key를 보안을 위해 다른 파일에서 임포트

--- Python/toy/dart/APIcallTest.py
+++ Python/toy/dart/APIcallTest.py
@@ -4,18 +4,25 @@ import zipfile
 import io
 import os, sys
 import pandas as pd
-#상위의 상위 폴더라서 두번 사용
-#KhAPI-Python-toy/APIcallTest.py
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
-from src.utils import get_project_root
+import tool.API_KEY
+from tool.utils import get_project_root
 
-FILENAME = ""
+# 한번이라면 
+# CORPCODE.xml 저장 경로: 
+# c:\KhAPI\Python\toy\data\dart\corpcode  
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+
+
+# CORPCODE.xml 파일을 저장할 경로, 프로젝트 최상위에 data폴더로 설정한다
 saveFileDir = f"{get_project_root()}\\data\\dart\\corpcode"
-print("회사코드 저장 경로: ", saveFileDir)
+print(" CORPCODE.xml 저장 경로: \n", saveFileDir)
 
 saveFileDir = "c:\KhAPI\data\dart\corpcode";
 API_KEY = "발급한 API 키를 입력하세요"
+API_KEY = tool.API_KEY
 
+# CORPCODE.xml을 다운로드 합니다.
 def down_corp_codeXML(API_KEY, saveFileDir):
     url = "https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key="
     with urlopen(url+API_KEY) as zipresp:
@@ -23,6 +30,10 @@ def down_corp_codeXML(API_KEY, saveFileDir):
             with zipfile.ZipFile(ioB) as zip:
                 zip.extractall(saveFileDir)
                 
+    
+# xml파일을 파싱한 결과를 list 형태로 리턴합니다.
+def parse_corp_code(saveFileDir):
+    # 아래는 파일 결과 예시:
     """
     <result>
         <list>
@@ -45,39 +56,38 @@ def down_corp_codeXML(API_KEY, saveFileDir):
         </list>
     </result>
     """
-
-
-def parse_corp_code(saveFileDir):
-    a = []
-    b = []
-    c = []
     
+    corp_code = []
+    stock_code = []
+    corp_name = []
+    
+    #다운받은 xml파일을 불러옵니다.
     xml_file = f"{saveFileDir}\CORPCODE.xml"
     print(xml_file)
     doc = ET.parse(xml_file).getroot()
     list_tg = doc.findall("list")
 
-    
+    # corp_code, stock_code, corp_name 각 배열에 항목을 추가합니다.
     for i in list_tg:
-        # if i.find("stock_code").text != " ":
-        #     print(i.find("stock_code").text)
-        a.append(i.find("corp_code").text)
-        c.append(i.find("corp_name").text)
+        corp_code.append(i.find("corp_code").text)
+        corp_name.append(i.find("corp_name").text)
+        
+        # stock_code가 빈 항목이 존재하기 때문에 임의로 지정했습니다.
         if i.find("stock_code").text == " ":
-            b.append("99999999")
+            stock_code.append("99999999")
         else:
-            b.append(i.find("stock_code").text)
+            stock_code.append(i.find("stock_code").text)
             
-    res = [a, b, c]
+    res = [corp_code, stock_code, corp_name]
     return res  
 
 
-abc = parse_corp_code(saveFileDir)
-res = {"corp_code": abc[0], "stock_code":abc[1], "corp_name":abc[2]}
 
-# for a in good:
-#     print(a)
+# parse_corp_code 함수 호출
+pars_res = parse_corp_code(saveFileDir)
 
-p1 = pd.DataFrame(res)
+corp_info = {"corp_code": pars_res[0], "stock_code":pars_res[1], "corp_name":pars_res[2]}
+
+p1 = pd.DataFrame(corp_info)
 
 print(p1['stock_code'].tail())

d5732fb158842ed62cb4bbe7e41c95afa8d60cf8
Gros23 <kjj6393@gmail.com>
2022-07-22 17:16:58 +0900

    한글 깨짐 문제 발견

--- fss/financial.py
+++ fss/financial.py
@@ -20,5 +20,18 @@ corp_code = '00126380'
 # fs = samsung.extract_fs(bgn_de='20120101') 와 동일
 fs = dart.fs.extract(corp_code=corp_code, bgn_de='20190101')
 #%%
+print(fs['is']["[D310000] Income statement, by function of expense - Consolidated financial statements (Unit: KRW)"]["label_ko"])
 
-print(fs['bs'].to_csv())
+
+# from flask import Flask  #서버 구현을 위한 Flask 객체 import
+# from flask_restx import Api, Resource #Api 구현을 위한 Api 객체 import
+#
+# app = Flask(__name__) #Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌
+# api = Api(app) #Flask 객체에 Api 객체 등록
+#
+# @api.route('/hello')
+# class HelloWorld(Resource):
+#     def get(self):
+#         return fs['cf']
+# if __name__=="__main__":
+#     app.run()
